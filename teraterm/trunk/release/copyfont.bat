fbf0fb62d22f3e4f6c399512d7d772dbf09668b9
yutakakn <yutakakn@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2004-12-15 14:31:31 +0000

    フォントのファイル属性の見直し。 TeraTerm関連プロセスをインストール前に殺すようにした。

--- teraterm/trunk/release/copyfont.bat
+++ teraterm/trunk/release/copyfont.bat
@@ -1,6 +1,8 @@
 @echo off
 set file=TSPECIAL1.TTF
 attrib -R %SystemRoot%\fonts\%file%
-attrib +R %file%
+attrib -R %file%
 copy /b /y %file% %SystemRoot%\fonts\
 attrib +R %SystemRoot%\fonts\%file%
+
+taskkill /f /im ttermpro.exe /im ttpmenu.exe /im LogMeIn.exe
