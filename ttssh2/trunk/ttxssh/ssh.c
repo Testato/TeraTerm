4835dcf4dabd3167e69a169718043199247addeb
yutakakn <yutakakn@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2004-12-17 16:52:36 +0000

    KEXにおけるRSAおよびDSSのkey verify処理を追加。

--- ttssh2/trunk/ttxssh/ssh.c
+++ ttssh2/trunk/ttxssh/ssh.c
@@ -2283,8 +2283,10 @@ void debug_print(int no, char *msg, int len)
 // クライアントからサーバへの提案事項
 #ifdef SSH2_DEBUG
 static char *myproposal[PROPOSAL_MAX] = {
-	"diffie-hellman-group14-sha1,diffie-hellman-group1-sha1,diffie-hellman-group-exchange-sha1",
-	"ssh-rsa,ssh-dss",
+	"diffie-hellman-group-exchange-sha1,diffie-hellman-group14-sha1,diffie-hellman-group1-sha1",
+//	"diffie-hellman-group14-sha1,diffie-hellman-group1-sha1,diffie-hellman-group-exchange-sha1",
+//	"ssh-rsa,ssh-dss",
+	"ssh-dss,ssh-rsa",
 	"3des-cbc,aes128-cbc",
 	"3des-cbc,aes128-cbc",
 	"hmac-md5,hmac-sha1",
@@ -3280,40 +3282,115 @@ void kex_derive_keys(PTInstVar pvar, int need, u_char *hash, BIGNUM *shared_secr
 }
 
 
+//////////////////////////////////////////////////////////////////////////////
+//
+// Key verify function
+// 
+//////////////////////////////////////////////////////////////////////////////
+
+//
+// DSS
+// 
+
+static int ssh_dss_verify(
+			DSA *key, u_char *signature, u_int signaturelen,
+			u_char *data, u_int datalen)
+{
+#define INTBLOB_LEN 20
+	DSA_SIG *sig;
+	const EVP_MD *evp_md = EVP_sha1();
+	EVP_MD_CTX md;
+	unsigned char digest[EVP_MAX_MD_SIZE], *sigblob;
+	unsigned int len, dlen;
+	int ret;
+	char *ptr;
+
+	OpenSSL_add_all_digests();
+
+	if (key == NULL) {
+		return -1;
+	}
+
+	ptr = signature;
+
+	// step1
+	len = get_uint32_MSBfirst(ptr);
+	ptr += 4;
+	if (strncmp("ssh-dss", ptr, len) != 0) {
+		return -1;
+	}
+	ptr += len;
+
+	// step2
+	len = get_uint32_MSBfirst(ptr);
+	ptr += 4;
+	sigblob = ptr;
+	ptr += len;
+
+	if (len != (2*INTBLOB_LEN)) {
+		return -1;
+	}
+
+	/* parse signature */
+	if ((sig = DSA_SIG_new()) == NULL)
+		return -1;
+	if ((sig->r = BN_new()) == NULL)
+		return -1;
+	if ((sig->s = BN_new()) == NULL)
+		return -1;
+	BN_bin2bn(sigblob, INTBLOB_LEN, sig->r);
+	BN_bin2bn(sigblob+ INTBLOB_LEN, INTBLOB_LEN, sig->s);
+
+	/* sha1 the data */
+	EVP_DigestInit(&md, evp_md);
+	EVP_DigestUpdate(&md, data, datalen);
+	EVP_DigestFinal(&md, digest, &dlen);
+
+	ret = DSA_do_verify(digest, dlen, sig, key);
+	memset(digest, 'd', sizeof(digest));
+
+	DSA_SIG_free(sig);
+
+	return ret;
+}
+
+
+//
+// RSA
+// 
 
 /*
- * See:
- * http://www.rsasecurity.com/rsalabs/pkcs/pkcs-1/
- * ftp://ftp.rsasecurity.com/pub/pkcs/pkcs-1/pkcs-1v2-1.asn
- */
+* See:
+* http://www.rsasecurity.com/rsalabs/pkcs/pkcs-1/
+* ftp://ftp.rsasecurity.com/pub/pkcs/pkcs-1/pkcs-1v2-1.asn
+*/
 /*
- * id-sha1 OBJECT IDENTIFIER ::= { iso(1) identified-organization(3)
- *	oiw(14) secsig(3) algorithms(2) 26 }
- */
+* id-sha1 OBJECT IDENTIFIER ::= { iso(1) identified-organization(3)
+*	oiw(14) secsig(3) algorithms(2) 26 }
+*/
 static const u_char id_sha1[] = {
 	0x30, 0x21, /* type Sequence, length 0x21 (33) */
-	0x30, 0x09, /* type Sequence, length 0x09 */
-	0x06, 0x05, /* type OID, length 0x05 */
-	0x2b, 0x0e, 0x03, 0x02, 0x1a, /* id-sha1 OID */
-	0x05, 0x00, /* NULL */
-	0x04, 0x14  /* Octet string, length 0x14 (20), followed by sha1 hash */
+		0x30, 0x09, /* type Sequence, length 0x09 */
+		0x06, 0x05, /* type OID, length 0x05 */
+		0x2b, 0x0e, 0x03, 0x02, 0x1a, /* id-sha1 OID */
+		0x05, 0x00, /* NULL */
+		0x04, 0x14  /* Octet string, length 0x14 (20), followed by sha1 hash */
 };
 /*
- * id-md5 OBJECT IDENTIFIER ::= { iso(1) member-body(2) us(840)
- *	rsadsi(113549) digestAlgorithm(2) 5 }
- */
+* id-md5 OBJECT IDENTIFIER ::= { iso(1) member-body(2) us(840)
+*	rsadsi(113549) digestAlgorithm(2) 5 }
+*/
 static const u_char id_md5[] = {
 	0x30, 0x20, /* type Sequence, length 0x20 (32) */
-	0x30, 0x0c, /* type Sequence, length 0x09 */
-	0x06, 0x08, /* type OID, length 0x05 */
-	0x2a, 0x86, 0x48, 0x86, 0xF7, 0x0D, 0x02, 0x05, /* id-md5 */
-	0x05, 0x00, /* NULL */
-	0x04, 0x10  /* Octet string, length 0x10 (16), followed by md5 hash */
+		0x30, 0x0c, /* type Sequence, length 0x09 */
+		0x06, 0x08, /* type OID, length 0x05 */
+		0x2a, 0x86, 0x48, 0x86, 0xF7, 0x0D, 0x02, 0x05, /* id-md5 */
+		0x05, 0x00, /* NULL */
+		0x04, 0x10  /* Octet string, length 0x10 (16), followed by md5 hash */
 };
 
-static int
-openssh_RSA_verify(int type, u_char *hash, u_int hashlen,
-    u_char *sigbuf, u_int siglen, RSA *rsa)
+static int openssh_RSA_verify(int type, u_char *hash, u_int hashlen,
+				   u_char *sigbuf, u_int siglen, RSA *rsa)
 {
 	u_int ret, rsasize, oidlen = 0, hlen = 0;
 	int len;
@@ -3350,39 +3427,40 @@ openssh_RSA_verify(int type, u_char *hash, u_int hashlen,
 		return 1; // error
 
 	if ((len = RSA_public_decrypt(siglen, sigbuf, decrypted, rsa,
-	    RSA_PKCS1_PADDING)) < 0) {
-		//error("RSA_public_decrypt failed: %s",
-		//    ERR_error_string(ERR_get_error(), NULL));
-		goto done;
-	}
-	if (len != hlen + oidlen) {
-		//error("bad decrypted len: %d != %d + %d", len, hlen, oidlen);
-		goto done;
-	}
-	if (memcmp(decrypted, oid, oidlen) != 0) {
-		//error("oid mismatch");
-		goto done;
-	}
-	if (memcmp(decrypted + oidlen, hash, hlen) != 0) {
-		//error("hash mismatch");
-		goto done;
-	}
-	ret = 1;
+		RSA_PKCS1_PADDING)) < 0) {
+			//error("RSA_public_decrypt failed: %s",
+			//    ERR_error_string(ERR_get_error(), NULL));
+			goto done;
+		}
+		if (len != hlen + oidlen) {
+			//error("bad decrypted len: %d != %d + %d", len, hlen, oidlen);
+			goto done;
+		}
+		if (memcmp(decrypted, oid, oidlen) != 0) {
+			//error("oid mismatch");
+			goto done;
+		}
+		if (memcmp(decrypted + oidlen, hash, hlen) != 0) {
+			//error("hash mismatch");
+			goto done;
+		}
+		ret = 1;
 done:
-	if (decrypted)
-		free(decrypted);
-	return ret;
+		if (decrypted)
+			free(decrypted);
+		return ret;
 }
 
-int ssh_rsa_verify(RSA *key, u_char *signature, u_int signaturelen,
-    u_char *data, u_int datalen)
+static int ssh_rsa_verify(RSA *key, u_char *signature, u_int signaturelen,
+				   u_char *data, u_int datalen)
 {
 	const EVP_MD *evp_md;
 	EVP_MD_CTX md;
-//	char *ktype;
+	//	char *ktype;
 	u_char digest[EVP_MAX_MD_SIZE], *sigblob;
 	u_int len, dlen, modlen;
-	int rlen, ret, nid;
+//	int rlen, ret, nid;
+	int ret, nid;
 	char *ptr;
 
 	OpenSSL_add_all_digests();
@@ -3395,7 +3473,7 @@ int ssh_rsa_verify(RSA *key, u_char *signature, u_int signaturelen,
 	}
 	//debug_print(41, signature, signaturelen);
 	ptr = signature;
-	
+
 	// step1
 	len = get_uint32_MSBfirst(ptr);
 	ptr += 4;
@@ -3409,10 +3487,12 @@ int ssh_rsa_verify(RSA *key, u_char *signature, u_int signaturelen,
 	ptr += 4;
 	sigblob = ptr;
 	ptr += len;
+#if 0
 	rlen = get_uint32_MSBfirst(ptr);
 	if (rlen != 0) {
 		return -1;
 	}
+#endif
 
 	/* RSA_verify expects a signature of RSA_size */
 	modlen = RSA_size(key);
@@ -3426,7 +3506,7 @@ int ssh_rsa_verify(RSA *key, u_char *signature, u_int signaturelen,
 		memset(sigblob, 0, diff);
 		len = modlen;
 	}
-//	nid = (datafellows & SSH_BUG_RSASIGMD5) ? NID_md5 : NID_sha1;
+	//	nid = (datafellows & SSH_BUG_RSASIGMD5) ? NID_md5 : NID_sha1;
 	nid = NID_sha1;
 	if ((evp_md = EVP_get_digestbynid(nid)) == NULL) {
 		//error("ssh_rsa_verify: EVP_get_digestbynid %d failed", nid);
@@ -3446,11 +3526,11 @@ int ssh_rsa_verify(RSA *key, u_char *signature, u_int signaturelen,
 	return ret;
 }
 
-int key_verify(
-	RSA *rsa_key, 
-	DSA *dsa_key,
-	unsigned char *signature, unsigned int signaturelen,
-	unsigned char *data, unsigned int datalen)
+static int key_verify(
+			   RSA *rsa_key, 
+			   DSA *dsa_key,
+			   unsigned char *signature, unsigned int signaturelen,
+			   unsigned char *data, unsigned int datalen)
 {
 	int ret = 0;
 
@@ -3458,8 +3538,7 @@ int key_verify(
 		ret = ssh_rsa_verify(rsa_key, signature, signaturelen, data, datalen);
 
 	} else if (dsa_key != NULL) {
-		// TODO:
-
+		ret = ssh_dss_verify(dsa_key, signature, signaturelen, data, datalen);
 
 	} else {
 		return -1;
@@ -3628,7 +3707,7 @@ static BOOL handle_SSH2_dh_kex_reply(PTInstVar pvar)
 		}
 	}
 
-#if 0
+#if 1
 	if (key_verify(rsa, dsa, signature, siglen, hash, 20) != 1) {
 		emsg = "key verify error @ handle_SSH2_dh_kex_reply()";
 		goto error;
@@ -3919,7 +3998,7 @@ static BOOL handle_SSH2_dh_gex_reply(PTInstVar pvar)
 		}
 	}
 
-#if 0
+#if 1
 	if (key_verify(rsa, dsa, signature, siglen, hash, 20) != 1) {
 		emsg = "key verify error @ handle_SSH2_dh_kex_reply()";
 		goto error;
@@ -4571,6 +4650,10 @@ static BOOL handle_SSH2_window_adjust(PTInstVar pvar)
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.7  2004/12/17 14:28:36  yutakakn
+ * メッセージ認証アルゴリズムに HMAC-MD5 を追加。
+ * TTSSHバージョンダイアログにHMACアルゴリズム表示を追加。
+ *
  * Revision 1.6  2004/12/17 14:05:55  yutakakn
  * パケット受信時のHMACチェックを追加。
  * KEXにおけるHMACアルゴリズムチェックを追加。
