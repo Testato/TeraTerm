67d8d5ff2470820c34cc87888dad47af513f79ba
yutakakn <yutakakn@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2005-02-19 07:02:59 +0000

    TitleFormatに 13 を追加。 COM5以上の表示に対応した。 キャプションバッファの拡張。

--- teraterm/trunk/source/teraterm/ttwinman.c
+++ teraterm/trunk/source/teraterm/ttwinman.c
@@ -41,58 +41,76 @@ void VTActivate()
   SetFocus(HVTWin);
 }
 
+// キャプションの変更
+//
+// (2005.2.19 yutaka) format ID=13の新規追加、COM5以上の表示に対応
 void ChangeTitle()
 {
-  int i;
-//  char TempTitle[HostNameMaxLength + 1]; // 81(yutaka)
-  char TempTitle[81]; // 81(yutaka)
-  char Num[11];
+	int i;
+	char TempTitle[HostNameMaxLength + 50 + 1]; // バッファ拡張
+	char Num[11];
 
-  strcpy(TempTitle, ts.Title);
+	strcpy(TempTitle, ts.Title);
 
-  if ((ts.TitleFormat & 1)!=0)
-  { // host name
-    strncat(TempTitle," - ",sizeof(TempTitle)-1-strlen(TempTitle));
-    i = sizeof(TempTitle)-1-strlen(TempTitle);
-    if (Connecting)
-      strncat(TempTitle,"[connecting...]",i);
-    else if (! cv.Ready)
-      strncat(TempTitle,"[disconnected]",i);
-    else if (cv.PortType==IdSerial)
-    {
-      switch (ts.ComPort) {
-	case 1: strncat(TempTitle,"COM1",i); break;
-	case 2: strncat(TempTitle,"COM2",i); break;
-	case 3: strncat(TempTitle,"COM3",i); break;
-	case 4: strncat(TempTitle,"COM4",i); break;
-      }
-    }
-    else
-      strncat(TempTitle,ts.HostName,i);
-  }
+	if ((ts.TitleFormat & 1)!=0)
+	{ // host name
+		strncat(TempTitle," - ",sizeof(TempTitle)-1-strlen(TempTitle));
+		i = sizeof(TempTitle)-1-strlen(TempTitle);
+		if (Connecting)
+			strncat(TempTitle,"[connecting...]",i);
+		else if (! cv.Ready)
+			strncat(TempTitle,"[disconnected]",i);
+		else if (cv.PortType==IdSerial)
+		{
+#if 1
+			// COM5 overに対応
+			char str[10];
+			_snprintf(str, sizeof(str), "COM%d", ts.ComPort);
+			strncat(TempTitle, str, i); 
 
-  if ((ts.TitleFormat & 2)!=0)
-  { // serial no.
-    strncat(TempTitle," (",sizeof(TempTitle)-1-strlen(TempTitle));
-    sprintf(Num,"%u",SerialNo);
-    strncat(TempTitle,Num,sizeof(TempTitle)-1-strlen(TempTitle));
-    strncat(TempTitle,")",sizeof(TempTitle)-1-strlen(TempTitle));
-  }
+#else
+			switch (ts.ComPort) {
+			case 1: strncat(TempTitle,"COM1",i); break;
+			case 2: strncat(TempTitle,"COM2",i); break;
+			case 3: strncat(TempTitle,"COM3",i); break;
+			case 4: strncat(TempTitle,"COM4",i); break;
+			}
+#endif
+		}
+		else {
+			if (ts.TitleFormat & 8) {
+				// format ID = 13(8 + 5): <hots/port> - <title>
+				_snprintf(TempTitle, sizeof(TempTitle), "%s - %s", ts.HostName, ts.Title);
 
-  if ((ts.TitleFormat & 4)!=0) // VT
-    strncat(TempTitle," VT",sizeof(TempTitle)-1-strlen(TempTitle));
-  SetWindowText(HVTWin,TempTitle);
+			} else {
+				strncat(TempTitle,ts.HostName,i);
+			}
+		}
+	}
 
-  if (HTEKWin!=0)
-  {
-    if ((ts.TitleFormat & 4)!=0) // TEK
-    {
-      TempTitle[strlen(TempTitle)-2] = 0;
-      strncat(TempTitle,"TEK",
-	sizeof(TempTitle)-1-strlen(TempTitle));
-    }
-    SetWindowText(HTEKWin,TempTitle);
-  }
+	if ((ts.TitleFormat & 2)!=0)
+	{ // serial no.
+		strncat(TempTitle," (",sizeof(TempTitle)-1-strlen(TempTitle));
+		sprintf(Num,"%u",SerialNo);
+		strncat(TempTitle,Num,sizeof(TempTitle)-1-strlen(TempTitle));
+		strncat(TempTitle,")",sizeof(TempTitle)-1-strlen(TempTitle));
+	}
+
+	if ((ts.TitleFormat & 4)!=0) // VT
+		strncat(TempTitle," VT",sizeof(TempTitle)-1-strlen(TempTitle));
+
+	SetWindowText(HVTWin,TempTitle);
+
+	if (HTEKWin!=0)
+	{
+		if ((ts.TitleFormat & 4)!=0) // TEK
+		{
+			TempTitle[strlen(TempTitle)-2] = 0;
+			strncat(TempTitle,"TEK",
+				sizeof(TempTitle)-1-strlen(TempTitle));
+		}
+		SetWindowText(HTEKWin,TempTitle);
+	}
 }
 
 void SwitchMenu()
@@ -147,3 +165,7 @@ void OpenHelp(HWND HWin, UINT Command, DWORD Data)
     strcat(HelpFN,HelpEng);
   WinHelp(HWin, HelpFN, Command, Data);
 }
+
+/*
+ * $Log: not supported by cvs2svn $
+ */
