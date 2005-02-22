c8cdf6c6d9dd32efc2e446fe270fae09f90bd3b6
yutakakn <yutakakn@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2005-02-22 12:53:25 +0000

    update

--- teraterm/trunk/installer/copyfile.bat
+++ teraterm/trunk/installer/copyfile.bat
@@ -1,13 +1,25 @@
+goto skip
+
 set dst="C:\Program Files\teraterm"
 copy /y C:\usr\cvs\teraterm\visualc\bin\release\*.exe %dst%
 copy /y C:\usr\cvs\teraterm\visualc\bin\release\*.dll %dst%
 copy /y C:\usr\cvs\ttssh2\ttxssh\Release\ttxssh.dll %dst%
 copy /y C:\usr\cvs\teraterm\source\ttmenu\Release\ttpmenu.exe %dst%
 
+:skip
 set dst="C:\usr\src\utf8_teraterm_installer"
-del /q %dst%\*.*
-copy /y C:\usr\cvs\teraterm\release\*.* %dst%
+del /s /q %dst%\*.*
 copy /y C:\usr\cvs\teraterm\visualc\bin\release\*.exe %dst%
 copy /y C:\usr\cvs\teraterm\visualc\bin\release\*.dll %dst%
 copy /y C:\usr\cvs\ttssh2\ttxssh\Release\ttxssh.dll %dst%
 copy /y C:\usr\cvs\teraterm\source\ttmenu\Release\ttpmenu.exe %dst%
+
+copy /y C:\usr\cvs\teraterm\release\*.* %dst%
+xcopy /s /e /y /i C:\usr\cvs\teraterm\release\theme %dst%\theme
+xcopy /s /e /y /i C:\usr\cvs\teraterm\release\plugin %dst%\plugin
+rmdir /s /q %dst%\plugin\CVS
+rmdir /s /q %dst%\theme\CVS
+rmdir /s /q %dst%\theme\scale\CVS
+rmdir /s /q %dst%\theme\tile\CVS
+
+pause
