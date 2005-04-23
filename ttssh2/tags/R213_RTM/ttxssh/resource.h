dade67b4c999f7b936440e5810d78f42dd6d160a
nobody <nobody@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2005-04-23 17:26:58 +0000

    This commit was manufactured by cvs2svn to create tag 'R213_RTM'.

---
+++ ttssh2/tags/R213_RTM/ttxssh/resource.h
@@ -0,0 +1,104 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ttxssh.rc
+//
+#define IDC_ADD                         3
+#define IDC_REMOVE                      4
+#define IDC_EDIT                        5
+#define IDI_SECURETT                    100
+#define IDD_ABOUTDIALOG                 101
+#define IDD_SSHSETUP                    102
+#define IDD_SSHUNKNOWNHOST              103
+#define IDD_SSHKEYGEN                   104
+#define IDD_SSHAUTH                     106
+#define IDD_HOSTDLG                     600
+#define IDC_HOSTTCPIP                   601
+#define IDD_SSHDIFFERENTHOST            601
+#define IDC_HOSTSERIAL                  602
+#define IDD_SSHAUTHSETUP                602
+#define IDD_SSHFWDSETUP                 603
+#define IDD_SSHFWDEDIT                  604
+#define IDD_SSHTISAUTH                  605
+#define IDC_HOSTCOMLABEL                608
+#define IDC_HOSTCOM                     609
+#define IDC_HOSTHELP                    699
+#define IDC_HOSTNAMELABEL               997
+#define IDC_HOSTNAME                    998
+#define IDC_SERVICELABEL                999
+#define IDC_SSHUSERNAME                 1000
+#define IDC_HOSTTELNET                  1000
+#define IDC_HOSTSSH                     1001
+#define IDC_SSHPASSWORD                 1002
+#define IDC_HOSTOTHER                   1002
+#define IDC_SSHAUTHBANNER               1003
+#define IDC_HOSTTCPPORTLABEL            1003
+#define IDC_SSHAUTHBANNER2              1004
+#define IDC_HOSTTCPPORT                 1004
+#define IDC_ABOUTTEXT                   1010
+#define IDC_SSHUSERNAMELABEL            1018
+#define IDC_READWRITEFILENAME           1022
+#define IDC_SSHCOMPRESSIONLEVEL         1023
+#define IDC_SSHCIPHERPREFS              1024
+#define IDC_SSHMOVECIPHERUP             1025
+#define IDC_SSHMOVECIPHERDOWN           1026
+#define IDC_ADDTOKNOWNHOSTS             1026
+#define IDC_CONTINUE                    1027
+#define IDC_READONLYFILENAME            1027
+#define IDC_HOSTWARNING                 1028
+#define IDC_CHOOSEREADWRITEFILE         1030
+#define IDC_CHOOSEREADONLYFILE          1031
+#define IDC_CHOOSERSAFILE               1031
+#define IDC_RSAFILENAME                 1032
+#define IDC_SSHFWDLIST                  1036
+#define IDC_SSHFWDX11                   1037
+#define IDC_SSHLTRFROMPORT              1038
+#define IDC_SSHLTRTOHOST                1039
+#define IDC_SSHLTRTOPORT                1040
+#define IDC_SSHRTLFROMPORT              1041
+#define IDC_SSHRTLTOHOST                1042
+#define IDC_SSHRTLTOPORT                1043
+#define IDC_SSHFWDLOCALTOREMOTE         1044
+#define IDC_SSHPASSWORDCAPTION          1045
+#define IDC_HOSTTCPPROTOCOLLABEL        1046
+#define IDC_HOSTTCPPROTOCOL             1047
+#define IDC_COMBO1                      1049
+#define IDC_SSH_VERSION                 1049
+#define IDC_SSH_VERSION_LABEL           1050
+#define IDC_OPENSSL_VERSION             1051
+#define IDC_HEARTBEATLABEL              1052
+#define IDC_HEARTBEAT_EDIT              1053
+#define IDC_HEARTBEATLABEL2             1054
+#define IDC_CHECK1                      1055
+#define IDC_KEYBOARD_INTERACTIVE_CHECK  1055
+#define IDC_TTSSH_VERSION               1056
+#define IDC_RSA1_TYPE                   1057
+#define IDC_RSA_TYPE                    1058
+#define IDC_DSA_TYPE                    1059
+#define IDC_KEYTYPE                     1060
+#define IDC_KEY_LABEL                   1061
+#define IDC_CONFIRM_LABEL               1062
+#define IDC_KEY_EDIT                    1063
+#define IDC_CONFIRM_EDIT                1064
+#define IDC_SAVE_PUBLIC_KEY             1065
+#define IDC_SAVE_PRIBATE_KEY            1066
+#define IDC_SAVE_PRIVATE_KEY            1066
+#define IDC_LOCALUSERNAMELABEL          1100
+#define IDC_LOCALUSERNAME               1101
+#define IDC_CHOOSEHOSTRSAFILE           1102
+#define IDC_HOSTRSAFILENAME             1103
+#define IDC_SSHUSEPASSWORD              1201
+#define IDC_SSHUSERSA                   1202
+#define IDC_SSHFWDREMOTETOLOCAL         1202
+#define IDC_SSHUSERHOSTS                1203
+#define IDC_SSHUSETIS                   1204
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1067
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
