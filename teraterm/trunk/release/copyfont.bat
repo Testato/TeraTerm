58d91b9561e3ce69df06fc41346bced9ad3706fe
yutakakn <yutakakn@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2005-08-14 16:25:59 +0000

    Windows9xでcopyコマンドがエラーとならないように、行末の\を削除した。

--- teraterm/trunk/release/copyfont.bat
+++ teraterm/trunk/release/copyfont.bat
@@ -2,7 +2,7 @@
 set file=TSPECIAL1.TTF 
 attrib -R %WinDir%\fonts\%file% 
 attrib -R %file% 
-copy /b /y %file% %WinDir%\fonts\ 
+copy /b /y %file% %WinDir%\fonts
 attrib +R %WinDir%\fonts\%file% 
 
 taskkill /f /im ttermpro.exe /im ttpmenu.exe /im LogMeTT.exe
