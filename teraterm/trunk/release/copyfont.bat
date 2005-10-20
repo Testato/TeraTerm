71396cd8c25ac294f4bbb8225a20ced39b7055b2
yutakakn <yutakakn@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2005-10-20 15:33:52 +0000

    Windows98上でコマンドエラーがでないようにした。

--- teraterm/trunk/release/copyfont.bat
+++ teraterm/trunk/release/copyfont.bat
@@ -1,8 +1,11 @@
 @echo off
+
 set file=TSPECIAL1.TTF 
-attrib -R %WinDir%\fonts\%file% 
-attrib -R %file% 
+
+if exist %WinDir%\fonts\%file% attrib -R %WinDir%\fonts\%file% 
+if exist %file% attrib -R %file% 
 copy /b /y %file% %WinDir%\fonts
 attrib +R %WinDir%\fonts\%file% 
 
-taskkill /f /im ttermpro.exe /im ttpmenu.exe /im LogMeTT.exe
+if exist %WinDir%\system32\taskkill.exe taskkill /f /im ttermpro.exe /im ttpmacro.exe /im ttpmenu.exe /im LogMeTT.exe /im LogMeTTc.exe /im cygterm.exe /im keycode.exe > NUL 2>&1
+
