3f449e87b2f759418a8475c595058d282e22facf
maya <maya@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2006-08-01 16:45:09 +0000

    ダイアログのリサイズを制限した。 リサイズ後にコントロールの位置を計算しなおすようにした。

--- teraterm/trunk/source/ttmacro/msgdlg.cpp
+++ teraterm/trunk/source/ttmacro/msgdlg.cpp
@@ -38,6 +38,7 @@ CMsgDlg::CMsgDlg(PCHAR Text, PCHAR Title, BOOL YesNo, int x, int y)
 
 BEGIN_MESSAGE_MAP(CMsgDlg, CDialog)
 	//{{AFX_MSG_MAP(CMsgDlg)
+	ON_MESSAGE(WM_EXITSIZEMOVE, OnExitSizeMove)
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
@@ -47,16 +48,12 @@ BOOL CMsgDlg::OnInitDialog()
 {
   RECT R;
   HDC TmpDC;
-  SIZE s;
-  HWND HText, HOk, HNo;
-  int WW, WH, CW, CH, TW, TH, BW, BH;
+  HWND HOk, HNo;
 
   CDialog::OnInitDialog();
   SetWindowText(TitleStr);
   SetDlgItemText(IDC_MSGTEXT,TextStr);
 
-  HText = ::GetDlgItem(GetSafeHwnd(), IDC_MSGTEXT);
-  
   TmpDC = ::GetDC(GetSafeHwnd());
   CalcTextExtent(TmpDC,TextStr,&s);
   ::ReleaseDC(GetSafeHwnd(),TmpDC);
@@ -72,22 +69,77 @@ BOOL CMsgDlg::OnInitDialog()
   GetWindowRect(&R);
   WW = R.right-R.left;
   WH = R.bottom-R.top;
+  
+  Relocation(TRUE, WW);
+
+#ifdef TERATERM32
+  SetForegroundWindow();
+#else
+  SetActiveWindow();
+#endif
+  return TRUE;
+}
+
+LONG CMsgDlg::OnExitSizeMove(UINT wParam, LONG lParam)
+{
+  RECT R;
+
+  GetWindowRect(&R);
+  if (R.bottom-R.top == WH && R.right-R.left == WW) {
+    // サイズが変わっていなければ何もしない
+  }
+  else if (R.bottom-R.top != WH || R.right-R.left < init_WW) {
+    // 高さが変更されたか、最初より幅が狭くなった場合は元に戻す
+    SetWindowPos(&wndTop,R.left,R.top,WW,WH,0);
+  }
+  else {
+    // そうでなければ再配置する
+    Relocation(FALSE, R.right-R.left);
+  }
+
+  return CDialog::DefWindowProc(WM_EXITSIZEMOVE,wParam,lParam);
+}
+
+void CMsgDlg::Relocation(BOOL is_init, int new_WW)
+{
+  RECT R;
+  HDC TmpDC;
+  HWND HText, HOk, HNo;
+  int CW, CH;
+
   GetClientRect(&R);
   CW = R.right-R.left;
   CH = R.bottom-R.top;
-  if (TW < CW)
+
+  // 初回のみ
+  if (is_init) {
+    // テキストコントロールサイズを補正
+    if (TW < CW)
+      TW = CW;
+    if (YesNoFlag && (TW < 7*BW/2))
+      TW = 7*BW/2;
+	// ウインドウサイズの計算
+    WW = WW + TW - CW;
+    WH = WH + 2*TH+3*BH/2 - CH;
+	init_WW = WW;
+  }
+  else {
     TW = CW;
-  if (YesNoFlag && (TW < 7*BW/2))
-    TW = 7*BW/2;
-  WW = WW + TW - CW;
-  WH = WH + 2*TH+3*BH/2 - CH;
+    WW = new_WW;
+  }
+
+  HText = ::GetDlgItem(GetSafeHwnd(), IDC_MSGTEXT);
+  HOk = ::GetDlgItem(GetSafeHwnd(), IDOK);
+  HNo = ::GetDlgItem(GetSafeHwnd(), IDCANCEL);
 
   ::MoveWindow(HText,(TW-s.cx)/2,TH/2,TW,TH,TRUE);
   if (YesNoFlag)
   {
-    ::SetWindowText(HOk,"&Yes");
+    if (is_init) {
+      ::SetWindowText(HOk,"&Yes");
+      ::SetWindowText(HNo,"&No");
+    }
     ::MoveWindow(HOk,(2*TW-5*BW)/4,2*TH,BW,BH,TRUE);
-    ::SetWindowText(HNo,"&No");
     ::MoveWindow(HNo,(2*TW+BW)/4,2*TH,BW,BH,TRUE);
     ::ShowWindow(HNo,SW_SHOW);
   }
@@ -102,10 +154,5 @@ BOOL CMsgDlg::OnInitDialog()
     ::ReleaseDC(GetSafeHwnd(),TmpDC);
   }
   SetWindowPos(&wndTop,PosX,PosY,WW,WH,0);
-#ifdef TERATERM32
-  SetForegroundWindow();
-#else
-  SetActiveWindow();
-#endif
-  return TRUE;
+  InvalidateRect(NULL);
 }
--- teraterm/trunk/source/ttmacro/msgdlg.h
+++ teraterm/trunk/source/ttmacro/msgdlg.h
@@ -19,10 +19,13 @@ public:
 protected:
 	PCHAR TextStr, TitleStr;
 	BOOL YesNoFlag;
-	int  PosX, PosY;
+	int  PosX, PosY, init_WW, WW, WH, TW, TH, BH, BW;
+	SIZE s;
 
 	//{{AFX_MSG(CMsgDlg)
 	virtual BOOL OnInitDialog();
+	afx_msg LONG OnExitSizeMove(UINT wParam, LONG lParam);
 	//}}AFX_MSG
+	void Relocation(BOOL is_init, int WW);
 	DECLARE_MESSAGE_MAP()
 };
