6f6dfa4a0c1aaf08640130ad55830736d253ef5d
nobody <nobody@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2006-08-18 16:01:38 +0000

    This commit was manufactured by cvs2svn to create tag 'R444_RTM'.

---
+++ teraterm/tags/R444_RTM/source/ttmacro/inpdlg.h
@@ -0,0 +1,34 @@
+/* Tera Term
+ Copyright(C) 1994-1998 T. Teranishi
+ All rights reserved. */
+
+/* TTMACRO.EXE, input dialog box */
+
+class CInpDlg : public CDialog
+{
+public:
+	CInpDlg(PCHAR Input, PCHAR Text, PCHAR Title,
+		BOOL Paswd, int x, int y);
+
+	//{{AFX_DATA(CInpDlg)
+	enum { IDD = IDD_INPDLG };
+	//}}AFX_DATA
+
+	//{{AFX_VIRTUAL(CInpDlg)
+	//}}AFX_VIRTUAL
+
+protected:
+	PCHAR InputStr, TextStr, TitleStr;
+	BOOL PaswdFlag;
+	int PosX, PosY, init_WW, WW, WH, TW, TH, BH, BW, EW, EH;
+	SIZE s;
+
+	//{{AFX_MSG(CInpDlg)
+	virtual BOOL OnInitDialog();
+	virtual void OnOK();
+	afx_msg LONG OnExitSizeMove(UINT wParam, LONG lParam);
+	//}}AFX_MSG
+	void Relocation(BOOL is_init, int WW);
+	DECLARE_MESSAGE_MAP()
+};
+typedef CInpDlg *PInpDlg;
