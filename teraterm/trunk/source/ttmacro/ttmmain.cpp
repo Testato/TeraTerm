e35b2d9fcdef18b8570189f1ea869d8af6094c6f
yutakapon <yutakapon@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2006-11-06 02:38:44 +0000

    100msecスリープは DDESend() で行うので削除。

--- teraterm/trunk/source/ttmacro/ttmmain.cpp
+++ teraterm/trunk/source/ttmacro/ttmmain.cpp
@@ -70,7 +70,11 @@ BOOL CCtrlWindow::OnIdle()
 	// 以下の暫定処置は復活。TeraTerm本体側のOnIdle()において、XTYP_ADVREQが頻繁に飛ぶことが
 	// CPUをストールさせてしまう要因と思われるのだが、根本的原因はいまだ不明。
 	// (2006.11.4 yutaka)
+	// 以下の暫定処置は DDESend() で行うため、削除。
+	// (2006.11.6 yutaka)
+#if 0
 	Sleep(100);
+#endif
     return TRUE;
   }
   else if (! Pause &&
