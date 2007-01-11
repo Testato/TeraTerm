66e17d5547d9fc45eb577a8cce4d079635aab518
nobody <nobody@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2007-01-11 12:27:20 +0000

    This commit was manufactured by cvs2svn to create tag 'ConnectingTimeout_after'.

---
+++ teraterm/tags/ConnectingTimeout_after/source/ttfile/bplus.h
@@ -0,0 +1,24 @@
+/* Tera Term
+ Copyright(C) 1994-1998 T. Teranishi
+ All rights reserved. */
+
+/* TTFILE.DLL, B-Plus protocol */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* prototypes */
+void BPInit
+  (PFileVar fv, PBPVar bv, PComVar cv, PTTSet ts);
+void BPTimeOutProc(PFileVar fv, PBPVar bv, PComVar cv);
+#ifdef I18N
+BOOL BPParse(PFileVar fv, PBPVar bv, PComVar cv, PTTSet ts);
+#else
+BOOL BPParse(PFileVar fv, PBPVar bv, PComVar cv);
+#endif
+void BPCancel(PBPVar bv);
+
+#ifdef __cplusplus
+}
+#endif
