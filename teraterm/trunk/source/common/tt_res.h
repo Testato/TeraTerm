972e24dab0e32521dda7214903d008c121db5e7c
maya <maya@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2007-06-03 05:03:00 +0000

    no message

--- teraterm/trunk/source/common/tt_res.h
+++ teraterm/trunk/source/common/tt_res.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by c:\works\ttssh2\teraterm\source\teraterm\ttermpro.rc
+// Used by c:\usr\cvs\teraterm\source\teraterm\ttermpro.rc
 //
 #define IDI_TTERM                       100
 #define IDR_ACC                         100
