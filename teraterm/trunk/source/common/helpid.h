3876d88b3501e7e919b22d862283870aff61c6b1
maya <maya@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2007-12-20 00:51:22 +0000

    no message

--- teraterm/trunk/source/common/helpid.h
+++ teraterm/trunk/source/common/helpid.h
@@ -172,6 +172,7 @@
 #define HlpMacroCommandClosett          92008
 #define HlpMacroCommandCode2str         92009
 #define HlpMacroCommandConnect          92010
+#define HlpMacroCommandCygConnect       92120
 #define HlpMacroCommandDelpassword      92011
 #define HlpMacroCommandDisconnect       92012
 #define HlpMacroCommandDo               92126
