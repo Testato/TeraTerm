8ddec45ebadb038e248fdde5394f70419e3932e5
maya <maya@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2008-02-14 10:21:44 +0000

    no message

--- teraterm/trunk/installer/convtext.bat
+++ teraterm/trunk/installer/convtext.bat
@@ -2,3 +2,5 @@ perl 2sjis.pl --i ..\source\oniguruma\doc\RE    --o ..\..\doc\en\html\reference\
 perl 2sjis.pl --i ..\source\oniguruma\doc\RE.ja --o ..\..\doc\jp\html\reference\RE.txt --c euc-jp --l unix
 perl 2sjis.pl --i ..\..\ttssh2\openssl\LICENSE  --o ..\..\doc\en\html\reference\OpenSSL-LICENSE.txt --c euc-jp --l unix
 perl 2sjis.pl --i ..\..\ttssh2\openssl\LICENSE  --o ..\..\doc\jp\html\reference\OpenSSL-LICENSE.txt --c euc-jp --l unix
+perl 2sjis.pl --i ..\..\ttssh2\putty\LICENCE  --o ..\..\doc\en\html\reference\PuTTY-LICENSE.txt --c shiftjis --l crlf
+perl 2sjis.pl --i ..\..\ttssh2\putty\LICENCE  --o ..\..\doc\jp\html\reference\PuTTY-LICENSE.txt --c shiftjis --l crlf
