2ac0348fe600398de24db0494460fc49995c56c2
doda <doda@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2008-03-29 12:04:29 +0000

    CygWinのperlでもビルド出来るように修正。 ディレクトリ区切り文字で/を使っていますが、手元では問題なくビルドできるようです。 何か問題があったら指摘してください。

--- teraterm/trunk/source/buildoniguruma.bat
+++ teraterm/trunk/source/buildoniguruma.bat
@@ -1,8 +1,8 @@
 cd oniguruma
 copy win32\Makefile Makefile
 copy win32\config.h config.h
-perl -e "open(IN,'win32\Makefile');while(<IN>){s/CFLAGS =/CFLAGS = \/MT/;print $_;}close(IN);" > Makefile
-perl -e "open(IN,'win32\Makefile');while(<IN>){s/CFLAGS =/CFLAGS = \/MTd/;s/libname   = /libname   = debug\\/;print $_;}close(IN);" > Makefile.debug
+perl -e "open(IN,'win32\Makefile');while(<IN>){s|CFLAGS =|CFLAGS = /MT|;print $_;}close(IN);" > Makefile
+perl -e "open(IN,'win32\Makefile');while(<IN>){s|CFLAGS =|CFLAGS = /MTd|;s|libname   = |libname   = debug/|;print $_;}close(IN);" > Makefile.debug
 mkdir debug
 nmake clean
 nmake -f Makefile.debug
