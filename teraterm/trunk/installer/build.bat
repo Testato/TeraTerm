e3f0ff0bf468560bcab852968bb2b298cbe45f31
maya <maya@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2008-04-14 17:35:50 +0000

    ファイル移動に伴う修正

--- teraterm/trunk/installer/build.bat
+++
@@ -1,9 +0,0 @@
-set BUILD=build
-if "%1" == "rebuild" (set BUILD=rebuild)
-
-devenv /%BUILD% release ..\visualc\ttermpro.sln
-devenv /%BUILD% release ..\..\ttssh2\ttssh.sln
-devenv /%BUILD% release ..\..\TTProxy\TTProxy.sln
-
-if "%1"=="plugins" devenv /%BUILD% release ..\visualc\TTXSamples.sln
-
