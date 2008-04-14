daaf3f16a6c6d1a1c28c5ef4a118b3a05c9fc753
maya <maya@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2008-04-14 17:27:55 +0000

    ファイル移動に伴う修正

--- installer/trunk/build.bat
+++ installer/trunk/build.bat
@@ -1,9 +1,10 @@
 set BUILD=build
 if "%1" == "rebuild" (set BUILD=rebuild)
 
-devenv /%BUILD% release ..\visualc\ttermpro.sln
-devenv /%BUILD% release ..\..\ttssh2\ttssh.sln
-devenv /%BUILD% release ..\..\TTProxy\TTProxy.sln
+devenv /%BUILD% release ..\teraterm\ttermpro.sln
+devenv /%BUILD% release ..\ttssh2\ttssh.sln
+devenv /%BUILD% release ..\TTProxy\TTProxy.sln
+devenv /%BUILD% release ..\TTXKanjiMenu\ttxkanjimenu.sln
+devenv /%BUILD% release ..\ttpmenu\ttpmenu.sln
 
 if "%1"=="plugins" devenv /%BUILD% release ..\visualc\TTXSamples.sln
-
--- installer/trunk/makearchive.bat
+++ installer/trunk/makearchive.bat
@@ -1,5 +1,7 @@
-CALL convtext.bat
+pushd ..\doc
 CALL makechm.bat
+popd
+
 CALL build.bat %1
 
 rem  for XP or later
@@ -14,31 +16,37 @@ if not exist %%a goto create
 del /s /q %dst%\*.*
 mkdir %dst%
 
-copy /y ..\..\teraterm\visualc\bin\release\*.exe %dst%
-copy /y ..\..\teraterm\visualc\bin\release\*.dll %dst%
+copy /y ..\teraterm\release\*.exe %dst%
+copy /y ..\teraterm\release\*.dll %dst%
+copy /y ..\ttssh2\ttxssh\Release\ttxssh.dll %dst%
+copy /y ..\cygterm\cygterm.exe %dst%
+copy /y ..\cygterm\cygterm.cfg %dst%
+copy /y ..\cygterm\cyglaunch.exe %dst%
+copy /y ..\ttpmenu\Release\ttpmenu.exe %dst%
+copy /y ..\TTProxy\Release\TTXProxy.dll %dst%
+copy /y ..\TTXKanjiMenu\Release\ttxkanjimenu.dll %dst%
+copy /y ..\TTXSamples\Release\*.dll %dst%
+
+rem Debug file
+if "%1"=="debug" copy /y ..\teraterm\release\*.pdb %dst%
+if "%1"=="debug" copy /y ..\ttssh2\ttxssh\Release\ttxssh.pdb %dst%
+if "%1"=="debug" copy /y ..\ttpmenu\Release\ttxssh.pdb %dst%
+if "%1"=="debug" copy /y ..\TTProxy\Release\TTXProxy.pdb %dst%
+if "%1"=="debug" copy /y ..\TTXKanjiMenu\Release\ttxkanjimenu.pdb %dst%
+if "%1"=="debug" copy /y ..\TTXSamples\Release\*.pdb %dst%
+
 pushd %dst%
 if exist TTXFixedWinSize.dll ren TTXFixedWinSize.dll _TTXFixedWinSize.dll
 popd
 
-copy /y ..\..\teraterm\cygterm\cygterm.exe %dst%
-copy /y ..\..\teraterm\cygterm\cygterm.cfg %dst%
-copy /y ..\..\teraterm\cygterm\cyglaunch.exe %dst%
+copy /y ..\doc\jp\teratermj.chm %dst%
+copy /y ..\doc\en\teraterm.chm %dst%
 
-rem Debug file
-if "%1"=="debug" copy /y ..\..\ttssh2\ttxssh\Release\ttxssh.pdb %dst%
-if "%1"=="debug" copy /y ..\..\teraterm\visualc\bin\release\*.pdb %dst%
-
-copy /y ..\..\ttssh2\ttxssh\Release\ttxssh.dll %dst%
-copy /y ..\..\TTProxy\Release\TTXProxy.dll %dst%
-copy /y ..\..\teraterm\source\ttmenu\Release\ttpmenu.exe %dst%
-copy /y ..\..\doc\jp\teratermj.chm %dst%
-copy /y ..\..\doc\en\teraterm.chm %dst%
-
-copy /y ..\..\teraterm\release\*.* %dst%
-xcopy /s /e /y /i ..\..\teraterm\release\theme %dst%\theme
-xcopy /s /e /y /i ..\..\teraterm\release\plugin %dst%\plugin
-xcopy /s /e /y /i ..\..\teraterm\release\Collector %dst%\Collector
-xcopy /s /e /y /i ..\..\teraterm\release\lang %dst%\lang
+copy /y release\*.* %dst%
+xcopy /s /e /y /i release\theme %dst%\theme
+xcopy /s /e /y /i release\plugin %dst%\plugin
+xcopy /s /e /y /i release\Collector %dst%\Collector
+xcopy /s /e /y /i release\lang %dst%\lang
 rmdir /s /q %dst%\plugin\CVS
 rmdir /s /q %dst%\theme\CVS
 rmdir /s /q %dst%\theme\scale\CVS
@@ -46,4 +54,4 @@ rmdir /s /q %dst%\theme\tile\CVS
 rmdir /s /q %dst%\lang\CVS
 del /f %dst%\lang\English.lng
 
-perl setini.pl ..\..\teraterm\release\TERATERM.INI > %dst%\TERATERM.INI
+perl setini.pl release\TERATERM.INI > %dst%\TERATERM.INI
