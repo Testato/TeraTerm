133e961bb48e678bc35ed753c3007edc937d8b7e
yutakapon <yutakapon@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2009-02-15 14:29:09 +0000

    Windows95で起動できなかったようなので、以下のサイトのコードをそのまま流用した。
    「かなり強引な方法で DLL のインポートを横取りする (未参照を防ぐ)」
    http://jet2.u-abel.net/program/tips/forceimp.htm
    http://jet2.u-abel.net/board.cgi?num=165

--- teraterm/trunk/teraterm/teraterm.cpp
+++ teraterm/trunk/teraterm/teraterm.cpp
@@ -43,10 +43,41 @@ CTeraApp theApp;
 // VS2005でビルドされたバイナリが Windows95 でも起動できるようにするために、
 // IsDebuggerPresent()のシンボル定義を追加する。
 // さらにWindows Sockets2.0のインストールも必要。
-EXTERN_C BOOL WINAPI _imp__IsDebuggerPresent(void)
+//
+// cf.http://jet2.u-abel.net/program/tips/forceimp.htm
+// 装飾された名前のアドレスを作るための仮定義
+// (これだけでインポートを横取りしている)
+EXTERN_C int WINAPI _imp__IsDebuggerPresent()
+    { return PtrToInt((void*) &_imp__IsDebuggerPresent); }
+// 実際に横取り処理を行う関数
+EXTERN_C BOOL WINAPI Cover_IsDebuggerPresent()
+    { return FALSE; }
+// 関数が実際に呼び出されたときに備えて
+// 横取り処理関数を呼び出させるための下準備
+EXTERN_C void __stdcall DoCover_IsDebuggerPresent()
 {
-	return FALSE;
+    DWORD dw;
+    DWORD_PTR FAR* lpdw;
+    // 横取り関数を設定するアドレスを取得
+    lpdw = (DWORD_PTR FAR*) &_imp__IsDebuggerPresent;
+    // このアドレスを書き込めるように設定
+    // (同じプログラム内なので障害なく行える)
+    VirtualProtect(lpdw, sizeof(DWORD_PTR), PAGE_READWRITE, &dw);
+    // 横取り関数を設定
+    *lpdw = (DWORD_PTR)(FARPROC) Cover_IsDebuggerPresent;
+    // 読み書きの状態を元に戻す
+    VirtualProtect(lpdw, sizeof(DWORD_PTR), dw, NULL);
 }
+// アプリケーションが初期化される前に下準備を呼び出す
+// ※ かなり早くに初期化したいときは、このコードを
+//  ファイルの末尾に書いて「#pragma init_seg(lib)」を、
+//  この変数宣言の手前に書きます。
+//  初期化を急ぐ必要が無い場合は WinMain 内から
+//  DoCover_IsDebuggerPresent を呼び出して構いません。
+EXTERN_C int s_DoCover_IsDebuggerPresent
+    = (int) (DoCover_IsDebuggerPresent(), 0);
+
+
 
 // CTeraApp initialization
 BOOL CTeraApp::InitInstance()
