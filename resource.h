987e54ca6b24a77a13cd7eb4739460555825a743
maya <maya@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2009-03-24 12:04:56 +0000

    CVS から SVN へ移行: 既存のタグ tags に集約

---
+++ resource.h
@@ -0,0 +1,79 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by ttx.rc
+//
+#define IDS_MENUCAPTION                 1
+#define IDS_ABOUT                       2
+#define IDD_ABOUTDIALOG                 100
+#define IDD_SETTING                     101
+#define IDD_OPTION_SETTING              102
+#define IDS_RESOLVE_AUTO                129
+#define IDS_RESOLVE_REMOTE              130
+#define IDS_RESOLVE_LOCAL               131
+#define IDS_EMPTY_HOSTNAME              132
+#define IDS_ILLEGAL_PORT                133
+#define IDS_EMPTY_PARAMETER             134
+#define IDS_PROXY_UNAUTHORIZED          135
+#define IDS_PROXY_BAD_REQUEST           136
+#define IDS_TYPE_NONE                   137
+#define IDS_LOGFILE_SELECT              138
+#define IDS_UNABLE_TO_GET_ISSUER_CERT   200
+#define IDS_UNABLE_TO_GET_CRL           201
+#define IDS_UNABLE_TO_DECRYPT_CERT_SIGNATURE 202
+#define IDS_UNABLE_TO_DECRYPT_CRL_SIGNATURE 203
+#define IDS_UNABLE_TO_DECODE_ISSUER_PUBLIC_KEY 204
+#define IDS_CERT_SIGNATURE_FAILURE      205
+#define IDS_CRL_SIGNATURE_FAILURE       206
+#define IDS_CERT_NOT_YET_VALID          207
+#define IDS_CERT_HAS_EXPIRED            208
+#define IDS_CRL_NOT_YET_VALID           209
+#define IDS_CRL_HAS_EXPIRED             210
+#define IDS_ERROR_IN_CERT_NOT_BEFORE_FIELD 211
+#define IDS_ERROR_IN_CERT_NOT_AFTER_FIELD 212
+#define IDS_ERROR_IN_CRL_LAST_UPDATE_FIELD 213
+#define IDS_ERROR_IN_CRL_NEXT_UPDATE_FIELD 214
+#define IDS_OUT_OF_MEM                  215
+#define IDS_DEPTH_ZERO_SELF_SIGNED_CERT 216
+#define IDS_SELF_SIGNED_CERT_IN_CHAIN   217
+#define IDS_UNABLE_TO_GET_ISSUER_CERT_LOCALLY 218
+#define IDS_UNABLE_TO_VERIFY_LEAF_SIGNATURE 219
+#define IDS_CERT_CHAIN_TOO_LONG         220
+#define IDS_CERT_REVOKED                221
+#define IDS_INVALID_CA                  222
+#define IDS_PATH_LENGTH_EXCEEDED        223
+#define IDS_INVALID_PURPOSE             224
+#define IDS_CERT_UNTRUSTED              225
+#define IDS_CERT_REJECTED               226
+#define IDS_SUBJECT_ISSUER_MISMATCH     227
+#define IDS_AKID_SKID_MISMATCH          228
+#define IDS_AKID_ISSUER_SERIAL_MISMATCH 229
+#define IDS_KEYUSAGE_NO_CERTSIGN        230
+#define IDS_APPLICATION_VERIFICATION    231
+#define IDS_UNMATCH_COMMON_NAME         232
+#define IDS_UNABLE_TO_GET_COMMON_NAME   233
+#define IDC_RESOLVE                     1000
+#define IDC_URL                         1000
+#define IDC_HOSTNAME                    1001
+#define IDC_USERNAME                    1002
+#define IDC_PASSWORD                    1003
+#define IDC_CONNECTED                   1004
+#define IDC_ERROR                       1005
+#define IDC_TIMEOUT                     1006
+#define IDC_TYPE                        1006
+#define IDC_SPIN_TIMEOUT                1007
+#define IDC_PORT                        1007
+#define IDC_OPTIONS                     1008
+#define IDC_REFER                       1010
+#define IDC_LOGFILE                     1012
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
