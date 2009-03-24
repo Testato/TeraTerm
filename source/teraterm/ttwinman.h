45081b0241afaf188f58dbe296efe52d15731d0e
maya <maya@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2009-03-24 12:04:56 +0000

    CVS から SVN へ移行: 既存のタグ tags に集約

---
+++ source/teraterm/ttwinman.h
@@ -0,0 +1,42 @@
+/* Tera Term
+ Copyright(C) 1994-1998 T. Teranishi
+ All rights reserved. */
+
+/* TERATERM.EXE, variables, flags related to VT win and TEK win */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* prototypes */
+void VTActivate();
+void ChangeTitle();
+void SwitchMenu();
+void SwitchTitleBar();
+void OpenHelp(HWND HWin, UINT Command, DWORD Data);
+void OpenHtmlHelp(HWND HWin, char *filename);
+
+extern HWND HVTWin;
+extern HWND HTEKWin;
+extern int ActiveWin; /* IdVT, IdTEK */
+extern int TalkStatus; /* IdTalkKeyb, IdTalkCB, IdTalkTextFile */
+extern BOOL KeybEnabled; /* keyboard switch */
+extern BOOL Connecting;
+
+/* 'help' button on dialog box */
+extern WORD MsgDlgHelp;
+extern LONG HelpId;
+
+extern TTTSet ts;
+extern TComVar cv;
+
+/* pointers to window objects */
+extern void* pVTWin;
+extern void* pTEKWin;
+/* instance handle */
+extern HINSTANCE hInst;
+
+extern int SerialNo;
+
+#ifdef __cplusplus
+}
+#endif
