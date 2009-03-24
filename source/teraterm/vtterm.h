1992d7ace0f7539d586536d1a1524b1de24e10a9
maya <maya@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2009-03-24 12:04:56 +0000

    CVS から SVN へ移行: 既存のタグ tags に集約

---
+++ source/teraterm/vtterm.h
@@ -0,0 +1,20 @@
+/* Tera Term
+ Copyright(C) 1994-1998 T. Teranishi
+ All rights reserved. */
+
+/* TERATERM.EXE, VT terminal emulation */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* prototypes */
+void ResetTerminal();
+void ResetCharSet();
+void ResetKeypadMode(BOOL DisabledModeOnly);
+void HideStatusLine();
+void ChangeTerminalSize(int Nx, int Ny);
+int VTParse();
+
+#ifdef __cplusplus
+}
+#endif
