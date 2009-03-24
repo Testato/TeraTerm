08cfd9533f258625792e98282f6f0d5a2c92f6d4
maya <maya@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2009-03-24 12:04:56 +0000

    CVS から SVN へ移行: 既存のタグ tags に集約

---
+++ source/common/ttlib.h
@@ -0,0 +1,33 @@
+/* Tera Term
+ Copyright(C) 1994-1998 T. Teranishi
+ All rights reserved. */
+
+/* useful routines */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+BOOL GetFileNamePos(PCHAR PathName, int far *DirLen, int far *FNPos);
+BOOL ExtractFileName(PCHAR PathName, PCHAR FileName);
+BOOL ExtractDirName(PCHAR PathName, PCHAR DirName);
+void FitFileName(PCHAR FileName, PCHAR DefExt);
+void AppendSlash(PCHAR Path);
+void Str2Hex(PCHAR Str, PCHAR Hex, int Len, int MaxHexLen, BOOL ConvSP);
+BYTE ConvHexChar(BYTE b);
+int Hex2Str(PCHAR Hex, PCHAR Str, int MaxLen);
+BOOL DoesFileExist(PCHAR FName);
+long GetFSize(PCHAR FName);
+void uint2str(UINT i, PCHAR Str, int len);
+#ifdef WIN32
+void QuoteFName(PCHAR FName);
+#endif
+int isInvalidFileNameChar(PCHAR FName);
+void deleteInvalidFileNameChar(PCHAR FName);
+int isInvalidStrftimeChar(PCHAR FName);
+int deleteInvalidStrftimeChar(PCHAR FName);
+void ParseStrftimeFileName(PCHAR FName);
+
+#ifdef __cplusplus
+}
+#endif
