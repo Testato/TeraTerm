dee5545699cfdfb494ff2689aa11f2d1d8b68282
maya <maya@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2009-03-24 12:04:56 +0000

    CVS から SVN へ移行: 既存のタグ tags に集約

---
+++ release/copyfont.bat
@@ -0,0 +1,8 @@
+@echo off
+set file=TSPECIAL1.TTF 
+attrib -R %WinDir%\fonts\%file% 
+attrib -R %file% 
+copy /b /y %file% %WinDir%\fonts\ 
+attrib +R %WinDir%\fonts\%file% 
+
+taskkill /f /im ttermpro.exe /im ttpmenu.exe /im LogMeTT.exe
