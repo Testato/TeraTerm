654b1959901fabf9130b3635092bc49bc7924acb
maya <maya@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2009-03-24 12:04:56 +0000

    CVS から SVN へ移行: 既存のタグ tags に集約

---
+++ source/common/helpid.h
@@ -0,0 +1,44 @@
+/* Tera Term
+ Copyright(C) 1994-1998 T. Teranishi
+ All rights reserved. */
+
+/* Help constants */
+
+/* help file names */
+#define HelpEng "ttermp.hlp"
+#define HelpJpn "ttermpj.hlp"
+
+/* HTML help file */
+#define HTML_HELP_EN "teraterm.chm"
+#define HTML_HELP_JP "teratermj.chm"
+
+/* help context IDs */
+#define HlpFileNewConnection 1
+#define HlpFileLog 2
+#define HlpFileSend 3
+#define HlpFileKmtGet 4
+#define HlpFileKmtSend 5
+#define HlpFileXmodemRecv 6
+#define HlpFileXmodemSend 7
+#define HlpFileZmodemSend 8
+#define HlpFileBPlusSend 9
+#define HlpFileQVSend 10
+#define HlpFileChangeDir 11
+#define HlpFilePrint 12
+#define HlpSetupTerminal 13
+#define HlpSetupTerminalRuss 14
+#define HlpSetupWindow 15
+#define HlpSetupFont 16
+#define HlpSetupFontRuss 17
+#define HlpSetupKeyboard 18
+#define HlpSetupKeyboardRuss 19
+#define HlpSetupSerialPort 20
+#define HlpSetupTCPIP 21
+#define HlpSetupGeneral 22
+#define HlpSetupSave 23
+#define HlpSetupRestore 24
+#define HlpSetupLoadKeyMap 25
+#define HlpWindowWindow 26
+#define HlpTEKFilePrint 27
+#define HlpTEKSetupWindow 28
+#define HlpTEKSetupFont 29
