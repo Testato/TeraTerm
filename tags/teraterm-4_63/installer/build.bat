211e4f29f10ad84bb81b4d7bdc7488c23c7640f0
maya <maya@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2009-07-05 14:01:56 +0000

    Release 4.63

---
+++ tags/teraterm-4_63/installer/build.bat
@@ -0,0 +1,32 @@
+set BUILD=build
+if "%1" == "rebuild" (set BUILD=rebuild)
+
+rem ライブラリをコンパイル
+pushd ..\libs
+CALL buildall.bat
+popd
+
+if "%BUILD%" == "rebuild" goto build
+
+rem "rebuild"を指定しない場合、SVNリビジョンを更新する。
+if exist ..\teraterm\release\svnrev.exe goto svnrev
+devenv /build release ..\teraterm\ttermpro.sln /project svnrev /projectconfig release
+
+:svnrev
+pushd ..\teraterm
+release\svnrev.exe > ttpdlg\svnversion.h
+popd
+
+:build
+devenv /%BUILD% release ..\teraterm\ttermpro.sln
+devenv /%BUILD% release ..\ttssh2\ttssh.sln
+devenv /%BUILD% release ..\TTProxy\TTProxy.sln
+devenv /%BUILD% release ..\TTXKanjiMenu\ttxkanjimenu.sln
+devenv /%BUILD% release ..\ttpmenu\ttpmenu.sln
+devenv /%BUILD% release ..\TTXSamples\TTXSamples.sln
+
+rem cygterm をコンパイル
+pushd ..\cygterm
+if "%BUILD%" == "rebuild" make clean
+make
+popd
