18e51ba3a996cc6e807c29860ced3e215333d983
yutakapon <yutakapon@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2012-10-17 10:50:16 +0000

    PuTTY / ssh.com形式の秘密鍵サポート用ブランチ

---
+++ branches/ssh_putty_pubkey/doc/makechm.bat
@@ -0,0 +1,22 @@
+
+if exist "%Programfiles(x86)%\HTML Help Workshop\hhc.exe" (
+	set HELP_COMPILER="%Programfiles(x86)%\HTML Help Workshop\hhc.exe"
+) else (
+	set HELP_COMPILER=C:\progra~1\htmlhe~1\hhc.exe
+)
+
+set updated=
+
+CALL convtext.bat
+
+for /f "delims=" %%i in ('perl htmlhelp_update_check.pl ja teratermj.chm') do @set updated=%%i
+if "%updated%"=="updated" (
+perl htmlhelp_index_make.pl ja html > ja\Index.hhk
+%HELP_COMPILER% ja\teraterm.hhp
+)
+
+for /f "delims=" %%i in ('perl htmlhelp_update_check.pl en teraterm.chm') do @set updated=%%i
+if "%updated%"=="updated" (
+perl htmlhelp_index_make.pl en html > en\Index.hhk
+%HELP_COMPILER% en\teraterm.hhp
+)
